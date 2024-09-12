e7473c1fc030dab8db6948079de5a45c6d837a08
Chriscodinghard <c.chowwww@gmail.com>
2024-09-12 16:28:11 -0700

    create calculate avg algo

---
+++ algo/dataAvg_algo.py
@@ -0,0 +1,80 @@
+# algorithm written in Python, based on Python v3.9
+
+import pandas as pd
+import numpy as np
+import os
+import time
+import json
+
+def get_job_details():
+    """Reads in metadata information about assets used by the algo"""
+    job = dict()
+    job['dids'] = json.loads(os.getenv('DIDS', None))
+    job['metadata'] = dict()
+    job['files'] = dict()
+    job['algo'] = dict()
+    job['secret'] = os.getenv('secret', None)
+    algo_did = os.getenv('TRANSFORMATION_DID', None)
+    if job['dids'] is not None:
+        for did in job['dids']:
+            # get the ddo from disk
+            filename = '/data/ddos/' + did
+            print(f'Reading json from {filename}')
+            with open(filename) as json_file:
+                ddo = json.load(json_file)
+                # search for metadata service
+                for service in ddo['service']:
+                    if service['type'] == 'metadata':
+                        job['files'][did] = list()
+                        index = 0
+                        for file in service['attributes']['main']['files']:
+                            job['files'][did].append(
+                                '/data/inputs/' + did + '/' + str(index))
+                            index = index + 1
+    if algo_did is not None:
+        job['algo']['did'] = algo_did
+        job['algo']['ddo_path'] = '/data/ddos/' + algo_did
+    return job
+
+
+def data_average(job_details):
+    """Executes the line counter based on inputs"""
+    print('Starting compute job with the following input information:')
+    print(json.dumps(job_details, sort_keys=True, indent=4))
+
+    """ Now, count the lines of the first file in first did """
+    first_did = job_details['dids'][0]
+    filename = job_details['files'][first_did][0]
+    non_blank_count = 0
+    with open(filename) as infp:
+        for line in infp:
+            if line.strip():
+                non_blank_count += 1
+    print ('number of non-blank lines found %d' % non_blank_count)
+
+    # Initialize DataFrame for column calculations
+    df = pd.read_csv(filename)
+    
+    """ Calculate average of numeric column """
+    avg_open = df['open'].mean() if 'open' in df.columns else None
+    avg_high = df['high'].mean() if 'high' in df.columns else None
+    avg_low = df['low'].mean() if 'low' in df.columns else None
+    avg_close = df['close'].mean() if 'close' in df.columns else None
+    print('Average Open:', avg_open)
+    print('Average High:', avg_high)
+    print('Average Low:', avg_low)
+    print('Average Close:', avg_close)
+
+    """ Print that number to output to generate algo output"""
+    with open("/data/outputs/result", "w") as f:
+        result = {
+            'non_blank_lines': non_blank_count,
+            'avg_open': avg_open,
+            'avg_high': avg_high,
+            'avg_low': avg_low,
+            'avg_close': avg_close
+        }
+        f.write(json.dumps(result, indent=4))
+
+if __name__ == '__main__':
+    data_average(get_job_details())
