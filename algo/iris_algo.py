ce53189ca91b0fc5cb1578db7547504e810bb41b
Chriscodinghard <c.chowwww@gmail.com>
2024-09-19 17:07:50 -0700

    change it to decentralize running root

--- algo/iris_algo.py
+++ algo/iris_algo.py
@@ -6,39 +6,7 @@ import os
 import time
 import json
 
-# def get_job_details():
-#     """Reads in metadata information about assets used by the algo"""
-#     job = dict()
-#     job['dids'] = json.loads(os.getenv('DIDS', None))
-#     job['metadata'] = dict()
-#     job['files'] = dict()
-#     job['algo'] = dict()
-#     job['secret'] = os.getenv('secret', None)
-#     algo_did = os.getenv('TRANSFORMATION_DID', None)
-#     if job['dids'] is not None:
-#         for did in job['dids']:
-#             # get the ddo from disk
-#             filename = '/data/ddos/' + did
-#             print(f'Reading json from {filename}')
-#             with open(filename) as json_file:
-#                 ddo = json.load(json_file)
-#                 # search for metadata service
-#                 for service in ddo['service']:
-#                     if service['type'] == 'metadata':
-#                         job['files'][did] = list()
-#                         index = 0
-#                         for file in service['attributes']['main']['files']:
-#                             job['files'][did].append(
-#                                 '/data/inputs/' + did + '/' + str(index))
-#                             index = index + 1
-#     if algo_did is not None:
-#         job['algo']['did'] = algo_did
-#         job['algo']['ddo_path'] = '/data/ddos/' + algo_did
-#     return job
-
 def get_job_details():
-    root = os.getenv('ROOT_FOLDER', '')
-    print('ROOT_FOLDER:', root)
     """Reads in metadata information about assets used by the algo"""
     job = dict()
     job['dids'] = json.loads(os.getenv('DIDS', None))
@@ -49,14 +17,46 @@ def get_job_details():
     algo_did = os.getenv('TRANSFORMATION_DID', None)
     if job['dids'] is not None:
         for did in job['dids']:
-            job['files'][did] = list()
-            # local path to the file
-            # Just one file for DID with name in the last ''  -> in this case "BTC_trend.json"
-            job['files'][did].append(root + '/data/inputs/' + did + '/iris.csv')
+            # get the ddo from disk
+            filename = '/data/ddos/' + did
+            print(f'Reading json from {filename}')
+            with open(filename) as json_file:
+                ddo = json.load(json_file)
+                # search for metadata service
+                for service in ddo['service']:
+                    if service['type'] == 'metadata':
+                        job['files'][did] = list()
+                        index = 0
+                        for file in service['attributes']['main']['files']:
+                            job['files'][did].append(
+                                '/data/inputs/' + did + '/' + str(index))
+                            index = index + 1
     if algo_did is not None:
         job['algo']['did'] = algo_did
-        job['algo']['ddo_path'] = root + '/data/ddos/' + algo_did
+        job['algo']['ddo_path'] = '/data/ddos/' + algo_did
     return job
+
+# def get_job_details():
+#     root = os.getenv('ROOT_FOLDER', '')
+#     print('ROOT_FOLDER:', root)
+#     """Reads in metadata information about assets used by the algo"""
+#     job = dict()
+#     job['dids'] = json.loads(os.getenv('DIDS', None))
+#     job['metadata'] = dict()
+#     job['files'] = dict()
+#     job['algo'] = dict()
+#     job['secret'] = os.getenv('secret', None)
+#     algo_did = os.getenv('TRANSFORMATION_DID', None)
+#     if job['dids'] is not None:
+#         for did in job['dids']:
+#             job['files'][did] = list()
+#             # local path to the file
+#             # Just one file for DID with name in the last ''  -> in this case "BTC_trend.json"
+#             job['files'][did].append(root + '/data/inputs/' + did + '/iris.csv')
+#     if algo_did is not None:
+#         job['algo']['did'] = algo_did
+#         job['algo']['ddo_path'] = root + '/data/ddos/' + algo_did
+#     return job
 
 
 def iris_algo(job_details):
