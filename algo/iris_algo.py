c8f6639081dc5f466fc09b4c791b6dbae8ac2d45
Chriscodinghard <c.chowwww@gmail.com>
2024-09-23 23:45:03 -0700

    update algo - problem still

--- algo/iris_algo.py
+++ algo/iris_algo.py
@@ -7,31 +7,9 @@ import time
 import json
 
 
-# def get_job_details():
-#     root = os.getenv('ROOT_FOLDER', '')
-#     print('ROOT_FOLDER:', root)
-#     """Reads in metadata information about assets used by the algo"""
-#     job = dict()
-#     job['dids'] = json.loads(os.getenv('DIDS', None))
-#     job['metadata'] = dict()
-#     job['files'] = dict()
-#     job['algo'] = dict()
-#     job['secret'] = os.getenv('secret', None)
-#     algo_did = os.getenv('TRANSFORMATION_DID', None)
-#     if job['dids'] is not None:
-#         for did in job['dids']:
-#             job['files'][did] = list()
-#             # local path to the file
-#             # Just one file for DID with name in the last ''  -> in this case "BTC_trend.json"
-#             job['files'][did].append(root + '/data/inputs/' + did + '/iris.csv')
-#     if algo_did is not None:
-#         job['algo']['did'] = algo_did
-#         job['algo']['ddo_path'] = root + '/data/ddos/' + algo_did
-#     return job
-
-
-
 def get_job_details():
+    root = os.getenv('ROOT_FOLDER', '')
+    print('ROOT_FOLDER:', root)
     """Reads in metadata information about assets used by the algo"""
     job = dict()
     job['dids'] = json.loads(os.getenv('DIDS', None))
@@ -42,24 +20,46 @@ def get_job_details():
     algo_did = os.getenv('TRANSFORMATION_DID', None)
     if job['dids'] is not None:
         for did in job['dids']:
-            # get the ddo from disk
-            filename = '/data/ddos/' + did
-            print(f'Reading json from {filename}')
-            with open(filename) as json_file:
-                ddo = json.load(json_file)
-                # search for metadata service
-                for service in ddo['service']:
-                    if service['type'] == 'metadata':
-                        job['files'][did] = list()
-                        index = 0
-                        for file in service['attributes']['main']['files']:
-                            job['files'][did].append(
-                                '/data/inputs/' + did + '/' + str(index))
-                            index = index + 1
+            job['files'][did] = list()
+            # local path to the file
+            # Just one file for DID with name in the last ''  -> in this case "BTC_trend.json"
+            job['files'][did].append(root + '/data/inputs/' + did + '/iris.csv')
     if algo_did is not None:
         job['algo']['did'] = algo_did
-        job['algo']['ddo_path'] = '/data/ddos/' + algo_did
+        job['algo']['ddo_path'] = root + '/data/ddos/' + algo_did
     return job
+
+
+
+# def get_job_details():
+#     """Reads in metadata information about assets used by the algo"""
+#     job = dict()
+#     job['dids'] = json.loads(os.getenv('DIDS', None))
+#     job['metadata'] = dict()
+#     job['files'] = dict()
+#     job['algo'] = dict()
+#     job['secret'] = os.getenv('secret', None)
+#     algo_did = os.getenv('TRANSFORMATION_DID', None)
+#     if job['dids'] is not None:
+#         for did in job['dids']:
+#             # get the ddo from disk
+#             filename = '/data/ddos/' + did
+#             print(f'Reading json from {filename}')
+#             with open(filename) as json_file:
+#                 ddo = json.load(json_file)
+#                 # search for metadata service
+#                 for service in ddo['service']:
+#                     if service['type'] == 'metadata':
+#                         job['files'][did] = list()
+#                         index = 0
+#                         for file in service['attributes']['main']['files']:
+#                             job['files'][did].append(
+#                                 '/data/inputs/' + did + '/' + str(index))
+#                             index = index + 1
+#     if algo_did is not None:
+#         job['algo']['did'] = algo_did
+#         job['algo']['ddo_path'] = '/data/ddos/' + algo_did
+#     return job
 
 
 def iris_algo(job_details):
@@ -97,7 +97,7 @@ def iris_algo(job_details):
             result[f'num_categories_{column}'] = df[column].nunique()
             print(f'Number of categories in {column}: {result[f"num_categories_{column}"]}')
 
-    with open("/data/outputs/result", "w") as f:
+    with open("data/outputs/result", "w") as f:
         for key, value in result.items():
             f.write(f'{key}: {value}\n')
 
--- algo/test/test_iris_algo.py
+++ algo/test/test_iris_algo.py
@@ -7,7 +7,7 @@ from algo.iris_algo import iris_algo, get_job_details
 
 class Test(TestCase):
 
-    @mock.patch.dict(os.environ, {"DIDS": " [ \"8f67E08be5dD941a701c2491E814535522c33bC2\" ]"})
+    @mock.patch.dict(os.environ, {"DIDS": " [ \"2f0530f8209fa324624d5e9bb741492b7c3a5c57c80b68cc01ffc0b3a8acecff\" ]"})
     @mock.patch.dict(os.environ, {"TRANSFORMATION_DID": "6EDaE15f7314dC306BB6C382517D374356E6B9De"})
     @mock.patch.dict(os.environ, {"secret": "MOCK-SECRET"})
     @mock.patch.dict(os.environ, {"ROOT_FOLDER": os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(__file__)), '../..'))})
