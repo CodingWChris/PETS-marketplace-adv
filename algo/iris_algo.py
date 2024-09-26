c74ec2a99838dcd93ccc350eab045588201009f3
Chriscodinghard <c.chowwww@gmail.com>
2024-09-26 10:51:31 -0700

    update get job detail

--- algo/iris_algo.py
+++ algo/iris_algo.py
@@ -18,24 +18,14 @@ def get_job_details():
     algo_did = os.getenv('TRANSFORMATION_DID', None)
     if job['dids'] is not None:
         for did in job['dids']:
-            # get the ddo from disk
-            filename = root + '/data/ddos/' + did
-            print(f'Reading json from {filename}')
-            with open(filename) as json_file:
-                ddo = json.load(json_file)
-                # search for metadata service
-                for service in ddo['service']:
-                    if service['type'] == 'metadata':
-                        job['files'][did] = list()
-                        index = 0
-                        for file in service['attributes']['main']['files']:
-                            job['files'][did].append(
-                                root + '/data/inputs/' + did + '/' + str(index))
-                            index = index + 1
+            job['files'][did] = list()
+            # Just one file for DID with name "0"
+            job['files'][did].append(root + '/data/inputs/' + did + '/0')
     if algo_did is not None:
         job['algo']['did'] = algo_did
         job['algo']['ddo_path'] = root + '/data/ddos/' + algo_did
     return job
+
 
 
 # def get_job_details():
@@ -68,67 +58,7 @@ def get_job_details():
 #         job['algo']['ddo_path'] = '/data/ddos/' + algo_did
 #     return job
 
-# def get_job_details():
-#     """Reads in metadata information about assets used by the algo"""
-#     job = dict()
-#     job['dids'] = json.loads(os.getenv('DIDS', None))
-#     job['metadata'] = dict()
-#     job['files'] = dict()
-#     job['algo'] = dict()
-#     job['secret'] = os.getenv('secret', None)
-#     algo_did = os.getenv('TRANSFORMATION_DID', None)
-#     if job['dids'] is not None:
-#         for did in job['dids']:
-#             # get the ddo from disk
-#             filename = '/data/ddos/' + did
-#             print(f'Reading json from {filename}')
-#             with open(filename) as json_file:
-#                 ddo = json.load(json_file)
-#                 # search for metadata service
-#                 for service in ddo['service']:
-#                     if service['type'] == 'metadata':
-#                         job['files'][did] = list()
-#                         index = 0
-#                         for file in service['attributes']['main']['files']:
-#                             job['files'][did].append(
-#                                 '/data/inputs/' + did + '/' + str(index))
-#                             index = index + 1
-#     if algo_did is not None:
-#         job['algo']['did'] = algo_did
-#         job['algo']['ddo_path'] = '/data/ddos/' + algo_did
-#     return job
 
-
-def get_job_details():
-    root = os.getenv('ROOT_FOLDER', '')
-    """Reads in metadata information about assets used by the algo"""
-    job = dict()
-    job['dids'] = json.loads(os.getenv('DIDS', None))
-    job['metadata'] = dict()
-    job['files'] = dict()
-    job['algo'] = dict()
-    job['secret'] = os.getenv('secret', None)
-    algo_did = os.getenv('TRANSFORMATION_DID', None)
-    if job['dids'] is not None:
-        for did in job['dids']:
-            # get the ddo from disk
-            filename = root + '/data/ddos/' + did
-            print(f'Reading json from {filename}')
-            with open(filename) as json_file:
-                ddo = json.load(json_file)
-                # search for metadata service
-                for service in ddo['service']:
-                    if service['type'] == 'metadata':
-                        job['files'][did] = list()
-                        index = 0
-                        for file in service['attributes']['main']['files']:
-                            job['files'][did].append(
-                                root + '/data/inputs/' + did + '/' + str(index))
-                            index = index + 1
-    if algo_did is not None:
-        job['algo']['did'] = algo_did
-        job['algo']['ddo_path'] = root + '/data/ddos/' + algo_did
-    return job
 
 
 
